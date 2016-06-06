cfa8a97b07d2a1cdc44bb6b87e2962d4cd77a8e9
uHOOCCOOHu <uHOOCCOOHu@users.noreply.github.com>
2016-06-06 12:13:12 +0800

    Rewrite and make it simple.

--- Main.hs
+++ Main.hs
@@ -2,12 +2,10 @@
 
 {-# LANGUAGE CPP        #-}
 {-# LANGUAGE DataKinds  #-}
-import System.Exit (die)
 import System.IO
-import System.IO.Error
 import System.Directory
 import System.Environment (getEnv)
-import Control.Exception (throw, catch, finally)
+import Control.Monad
 import Paths_Haskell_Console (version)
 import Data.Version (showVersion)
 import qualified Network.HTTP.Client as H
@@ -15,113 +13,66 @@ import qualified Network.HTTP.Client.TLS as TLS
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as BL (toStrict)
 
-strBanner =
-  unlines [ "HostsTool-Console " ++ showVersion version ++ " for racaljk/hosts"
-          , "  Powered By: uHOOCCOOHu"
-          , "(https://github.com/HostsTools/cross-platform-console)"
-          ]
-strUpdating =
-  unlines [ "Updating hosts..."
-          , "正在更新Hosts..."
-          ]
-strUpdated =
-  unlines [ "Hosts updated successfully."
-          , "Hosts更新成功。"
-          ]
-strAlreadyLatest =
-  unlines [ "Nothing was done. Hosts are already the latest version."
-          , "Hosts已是最新版本，无需更新。"
-          ]
-strNoPermission =
-  unlines [ "Permission denied. "
-          , "Please run with sudo(linux) or administrator privileges(windows)."
-          , "没有权限。"
-          , "请使用sudu(linux)或管理员权限（windows）运行本程序。"
-          ]
-strNetError =
-  unlines [ "Network error. Please check network setting and retry."
-          , "网络错误。请检查网络配置并重试。"
-          ]
-strFileInUse =
-  unlines [ "Hosts file is already in use. Please check your applications."
-          , "Hosts文件正在被使用，请检查是否有其他软件正在编辑它。"
-          ]
-strPause =
-  unlines [ "Press enter to exit..."
-          , "按回车键退出..."
-          ]
+strBanner :: String
+strNoUpdate :: String
+strBanner = unlines
+            [ "HostsTool-Console " ++ showVersion version ++ " for racaljk/hosts"
+            , "  Powered By: uHOOCCOOHu"
+            , "(https://github.com/HostsTools/cross-platform-console)"
+            ]
+strUpdated  = "Done."
+strNoUpdate = "Nothing was done. Hosts are already the latest version."
 
-strTargetUrl = "https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
+strHostsUrl :: String
+strHostsBeginMark :: B.ByteString
+strHostsUrl = "https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
+strHostsBeginMark = B.pack "# Copyright (c) 2014-2016, racaljk."
 
-strHostsBeginMark = "# Copyright (c) 2014-2016, racaljk."
-
-getHostsPath :: IO String
+getHostsPath :: IO FilePath
 #if defined(cygwin32_HOST_OS) || defined (mingw32_HOST_OS)
-getHostsPath = do
-  syspath <- getEnv "SystemRoot"
-  return $ syspath ++ "/system32/drivers/etc/hosts"
+getHostsPath = (++ "/system32/drivers/etc/hosts") <$> getEnv "SystemRoot"
 #else
 getHostsPath = return "/etc/hosts"
 #endif
 
-main :: IO ()
-main = finally main' (putStrLn strPause >> getChar >> return ())
-
 main' :: IO ()
 main' = do
-  hSetEncoding stdout localeEncoding
   putStrLn strBanner
-  putStrLn strUpdating
-  updated <- (addHostsRWPermission >> updateHosts) `catch` onIOError
-                                                   `catch` onHttpError
-  putStrLn $ if updated then strUpdated else strAlreadyLatest
-  where onIOError e
-          | isPermissionError e   = die strNoPermission
-          | isAlreadyInUseError e = die strFileInUse
-          | otherwise             = die $ show e
-        onHttpError = die . (strNetError ++) . show :: H.HttpException -> IO a
+  hostspath <- getHostsPath
+  ensureRWPermission hostspath
+  newhosts <- fetchURL strHostsUrl
+  updated <- updateHosts hostspath newhosts
+  putStrLn $ if updated then strUpdated else strNoUpdate
 
-addHostsRWPermission :: IO ()
-addHostsRWPermission = do
-  path <- getHostsPath
+ensureRWPermission :: FilePath -> IO ()
+ensureRWPermission path = do
   per <- getPermissions path
-  if readable per && writable per then
-    return ()
-  else
+  unless (readable per && writable per) $
     setPermissions path per{readable = True, writable = True}
 
-updateHosts :: IO Bool
-updateHosts = getHostsPath >>= updateHosts'
-  where updateHosts' path = withFile path ReadWriteMode process
-        process hfile = do
+updateHosts :: FilePath -> B.ByteString -> IO Bool
+updateHosts path new = withFile path ReadWriteMode process
+  where process hfile = do
           size <- hFileSize hfile
           old <- B.hGet hfile (fromIntegral size)
-          new <- fetchHosts
-          case transformHosts old new of
-            Nothing -> return False
-            Just content -> do
-              hSetFileSize hfile 0
-              hSeek hfile AbsoluteSeek 0
-              B.hPut hfile content
-              return True
-
-transformHosts :: B.ByteString -> B.ByteString -> Maybe B.ByteString
-transformHosts oldcontent newcontent
-  | r == newcontent = Nothing
-  | otherwise       = Just $ B.concat
-    [ l
-    , supplyLn l 3
-    , newcontent
-    ]
-  where (l, r) = B.breakSubstring (B.pack strHostsBeginMark) oldcontent
+          let (l, r) = B.breakSubstring strHostsBeginMark old
+          if r == new then
+            return False
+          else do
+            let llen = fromIntegral $ B.length l
+            hSetFileSize hfile llen
+            hSeek hfile AbsoluteSeek llen
+            B.hPut hfile $ supplyLn l 3
+            B.hPut hfile new
+            return True
 
 supplyLn :: B.ByteString -> Int -> B.ByteString
 supplyLn s n = repn $ (n-) $ B.length $ snd $ B.spanEnd (== '\n') s
   where repn n = B.replicate (max 0 n) '\n'
 
-fetchHosts :: IO B.ByteString
-fetchHosts = do
-  request <- H.parseUrl strTargetUrl
+fetchURL :: String -> IO B.ByteString
+fetchURL url = do
+  request <- H.parseUrl url
   manager <- H.newManager TLS.tlsManagerSettings
   response <- H.httpLbs request manager
   return $ BL.toStrict $ H.responseBody response
