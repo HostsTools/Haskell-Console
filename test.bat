fb6623f2c614f517f560c3563af0d7406a05a39f
uHOOCCOOHu <uHOOCCOOHu@users.noreply.github.com>
2016-06-08 19:48:31 +0800

    Edit appveyor settings.

--- test.bat
+++
@@ -1,4 +0,0 @@
-set path=%path%;c:\mingw\bin;
-appveyor DownloadFile "https://raw.githubusercontent.com/HostsTools/test-tool/master/haskellconsoleruntest.cpp" -FileName "runtest.cpp" 
-g++ -o runtest.exe runtest.cpp -DONLINERUN
-runtest "C:\projects\haskell-console\.stack-work"
